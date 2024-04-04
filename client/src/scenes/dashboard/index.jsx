563a3311656cf249c35a3c0a1c93887a7a7d0cf4
Joelamar <143662437+Josiaslamar@users.noreply.github.com>
2024-04-04 22:40:30 +0200

    joe

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -24,9 +24,41 @@ const Dashboard = () => {
   const theme = useTheme();
   const isNonMediumScreens = useMediaQuery("(min-width: 1200px)");
   const { data, isLoading } = useGetDashboardQuery();
-  console.log("data", data);
+  
 
-  return <div>Dashboard</div>;
-};
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "userId",
+      headerName: "User ID",
+      flex: 1,
+    },
+    {
+      field: "createdAt",
+      headerName: "CreatedAt",
+      flex: 1,
+    },
+    {
+      field: "products",
+      headerName: "# of Products",
+      flex: 0.5,
+      sortable: false,
+      renderCell: (params) => params.value.length,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
+    },
+  ];
+
+  return <Box m="1.5rem">
+
+  </Box>
 
 export default Dashboard;
