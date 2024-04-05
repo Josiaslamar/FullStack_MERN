3835947c2b070b599d38fd4733f71ce70460608e
Joelamar <143662437+Josiaslamar@users.noreply.github.com>
2024-04-05 14:42:10 +0200

    init commit

--- client/src/components/OverviewChart.jsx
+++ client/src/components/OverviewChart.jsx
@@ -92,6 +92,7 @@ const OverviewChart = ({ isDashboard = false, view }) => {
       }}
       yFormat=" >-.2f"
       curve="catmullRom"
+      enableArea={isDashboard}
       axisTop={null}
       axisRight={null}
       axisBottom={{
@@ -109,6 +110,7 @@ const OverviewChart = ({ isDashboard = false, view }) => {
       }}
       axisLeft={{
         orient: "left",
+        tickValues: 5,
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
