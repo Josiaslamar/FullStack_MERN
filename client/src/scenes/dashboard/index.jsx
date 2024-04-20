8099fc468a1fde34c6b5778df162fcffa1b9b100
Joelamar <143662437+Josiaslamar@users.noreply.github.com>
2024-04-20 15:13:33 +0200

    jo

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -61,7 +61,6 @@ const Dashboard = () => {
     <Box m="1.5rem 2.5rem">
       <FlexBetween>
         <Header title="Dashboard" subtitle="Welcome to your dashboard" />
-
         <Box>
           <Button
             sx={{
